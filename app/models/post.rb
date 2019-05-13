e2e15fcbd80ea76c762efcf2197a1fc52cba380e
Masayoshi <fujita.masayoshi@gmail.com>
2019-05-13 13:59:02 +0900

    複数画像投稿の準備
    - 複数画像投稿の準備
      - Edit /views/posts/new.html.erb
      - Add “dependent: :destroy”
      - Add Jquery

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@
 
 class Post < ApplicationRecord
   belongs_to :user
-  has_many :photos
+  has_many :photos, dependent: :destroy  # postが削除された時にphotoも削除される
   has_many :likes, -> { order(created_at: :desc) }, dependent: :destroy
   has_many :comments, dependent: :destroy
 
